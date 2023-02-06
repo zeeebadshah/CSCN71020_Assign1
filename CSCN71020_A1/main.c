28d9abd4de758e1c2ab1c4200469b3accc0e99b7
zeeebadshah <mbadshah4394@conestogac.on.ca>
2023-02-06 17:31:52 -0500

    Num 3 was added on to addition function

--- CSCN71020_A1/main.c
+++ CSCN71020_A1/main.c
@@ -41,15 +41,15 @@ void printOptions() {
 }
 
 void add() {
-	double num1, num2, num3, result;
+	double num1, num2, num3 , result;
 	printf("Enter the first value:");
 	scanf_s("%lf", &num1);
 	printf("Enter the second value:");
 	scanf_s("%lf", &num2);
-	printf("Enter the third value:");
+	printf("Enter the third value: ");
 	scanf_s("%lf", &num3);
 	result = num1 + num2 + num3;
-	printf("%lf + %lf + %lf = %lf\n", num1, num2, result);
+	printf("%lf + %lf + %lf = %lf\n", num1, num2 , num3 , result);
 }
 void sub() {
 	double num1, num2, num3, result;
